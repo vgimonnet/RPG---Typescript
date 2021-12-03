d271b8bdf2d4125bf131ed9fa6819b93f4dcb9c5
vgimonnet <valentin.gimonnet18@gmail.com>
2021-12-03 16:35:59 +0100

    Sorcerer cannot heal himself if is dead

--- src/class/sorcerer.class.ts
+++ src/class/sorcerer.class.ts
@@ -2,7 +2,9 @@ import Champion from "./champion.class";
 
 export default class Sorcerer extends Champion {
   heal(): void {
-    if (this.health + 10 < this.maxHealth) {
+    if (this.health <= 0) {
+      console.log('You\'re dead, you cannot heal')
+    } else if (this.health + 10 < this.maxHealth) {
       this.health += 10;
     } else {
       this.health = this.maxHealth;
